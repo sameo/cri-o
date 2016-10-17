79a41a9668f87502fa762148abaaa9d7c826e5be
Nalin Dahyabhai <nalin@redhat.com>
2016-10-17 09:53:40 -0400

    Vendor containers/image and containers/storage
    Vendor updated containers/image and containers/storage, along
    with any new dependencies they drag in, and updated versions of other
    dependencies that happen to get pulled in.
    
    github.com/coreos/go-systemd/daemon/SdNotify() now takes a boolean to
    control whether or not it unsets the NOTIFY_SOCKET variable from the
    calling process's environment.  Adapt.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- vendor/src/github.com/containers/image/docker/reference/reference.go
+++ vendor/src/github.com/containers/image/docker/reference/reference.go
@@ -1,11 +1,14 @@
 package reference
 
 import (
-	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 
+	"github.com/pkg/errors"
+
+	// "docker/distribution/digest" requires us to load the algorithms that we
+	// want to use into the binary (it calls .Available).
+	_ "crypto/sha256"
 	"github.com/docker/distribution/digest"
 	distreference "github.com/docker/distribution/reference"
 )
@@ -55,7 +58,7 @@ type Canonical interface {
 func ParseNamed(s string) (Named, error) {
 	named, err := distreference.ParseNamed(s)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing reference: %q is not a valid repository/tag", s)
+		return nil, errors.Wrapf(err, "Error parsing reference: %q is not a valid repository/tag", s)
 	}
 	r, err := WithName(named.Name())
 	if err != nil {
@@ -207,14 +210,14 @@ var validHex = regexp.MustCompile(`^([a-f0-9]{64})$`)
 
 func validateID(id string) error {
 	if ok := validHex.MatchString(id); !ok {
-		return fmt.Errorf("image ID %q is invalid", id)
+		return errors.Errorf("image ID %q is invalid", id)
 	}
 	return nil
 }
 
 func validateName(name string) error {
 	if err := validateID(name); err == nil {
-		return fmt.Errorf("Invalid repository name (%s), cannot specify 64-byte hexadecimal strings", name)
+		return errors.Errorf("Invalid repository name (%s), cannot specify 64-byte hexadecimal strings", name)
 	}
 	return nil
 }
